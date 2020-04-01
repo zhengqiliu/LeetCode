85da3725178b5ceb96afeaa8364f622e618f41a7
zhengqiliu <38197292+zhengqiliu@users.noreply.github.com>
2020-03-31 18:42:25 -0700

    Update 40. Combination Sum II.py

--- 40. Combination Sum II.py
+++ 40. Combination Sum II.py
@@ -1,16 +1,20 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        res = set()
+        res = []
         def dfs(index, target, path):
             if target < 0:
                 return
             if target == 0:
-                res.add(tuple(path))
+                res.append(path)
                 return
             for i in range(index, len(candidates)):
+                if i > index and candidates[i-1] == candidates[i]:
+                    continue
+                if candidates[i] > target:
+                    return
                 dfs(i+1, target - candidates[i], path + [candidates[i]])
         
         dfs(0, target, [])
         
-        return list(res)
+        return res
