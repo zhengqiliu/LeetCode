515a9e546c6a3db0923c30b53b8322af59665fb4
zhengqiliu <38197292+zhengqiliu@users.noreply.github.com>
2020-04-03 11:30:01 -0700

    Create 58. Length of Last Word.py

---
+++ 58. Length of Last Word.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        for word in reversed(s.split(' ')):
+            if word:
+                return len(word)
+        
+        return 0
+        
