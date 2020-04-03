8ff6350b0bcdbf0c8be66e17db3ee98ab0df84a0
zhengqiliu <38197292+zhengqiliu@users.noreply.github.com>
2020-04-03 15:15:22 -0700

    Update 62. Unique Paths.py

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[0]*n for _ in range(m)]
@@ -14,3 +15,24 @@ class Solution:
                     dp[i][j] = dp[i][j-1] + dp[i-1][j]
         
         return dp[-1][-1]
+
+    
+# Solution 2
+# Math Solution
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        left = m-1
+        right = n-1
+        
+        def factorial(n):
+            if n == 0:
+                return 1
+            s = 1
+            while n > 1:
+                s *= n
+                n -= 1
+            
+            return s
+        
+        return factorial(left + right) // (factorial(left) * factorial(right))
+        
