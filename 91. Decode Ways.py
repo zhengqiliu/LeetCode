23683593b473381fb35192c00f35e42cccb73fb2
zhengqiliu <38197292+zhengqiliu@users.noreply.github.com>
2020-04-07 10:25:36 -0700

    Create 91. Decode Ways.py

---
+++ 91. Decode Ways.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0]*(len(s) +1)
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '0' else 0
+        
+        for i in range(2, len(s)+1):
+            if 0 < int(s[i-1:i]) <= 9:
+                dp[i] += dp[i-1]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+        
+        return dp[-1]
